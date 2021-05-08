282fd80fdb1fc8dd9c34a6609909803e99a0136b
jaspreet <jaspreetjhass91@gmail.com>
2021-05-08 16:54:08 +0530

    initial code

---
+++ src/main/java/org/jp/spring/security/in/memory/SpringSecurityInMemoryAuthApplication.java
@@ -0,0 +1,13 @@
+package org.jp.spring.security.in.memory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityInMemoryAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityInMemoryAuthApplication.class, args);
+	}
+
+}
